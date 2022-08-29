4a673f10213bdbcda7ed5594ced517ed7ca6ff1c
christianlund <christian.lund0890@gmail.com>
2022-08-29 09:53:54 +0000

    Add map API variables and adjust html display

---
+++ assets/js/map.js
@@ -0,0 +1,10 @@
+// function initMap() {
+//     var map = new google.maps.Map(document.getElementById("map"), {
+//         center: {
+//             lat: 59.9260851,
+//             lng: 10.7526624
+//         },
+                
+//         zoom: 12
+//     });
+// }
